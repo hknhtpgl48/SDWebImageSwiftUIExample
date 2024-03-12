32b1f1d84e5b7c2ff687f7bc47fa7638ce824aea
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-03-12 15:25:03 +0300

    Initial Commit

---
+++ SDWebImageSwiftUIExample/SDWebImageSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDWebImageSwiftUIExampleApp.swift
+//  SDWebImageSwiftUIExample
+//
+//  Created by Hakan Hatipoğlu on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SDWebImageSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
